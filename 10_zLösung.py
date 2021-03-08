e252e8aa31421767a4f6b75e29de7ae93085f8b4
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-08 21:29:36 +0100

    Create 10_zLösung.py

---
+++ 10_zLösung.py
@@ -0,0 +1,8 @@
+print("Lösung:")
+nummer= int(input("Gib hier eine Zahl ein: "))
+if nummer%2==0 and nummer%3==0:
+  print("Die eingegebene Zahl " + str(nummer) +" ist durch zwei und drei ohne Rest teilbar und somit auch ohne Rest durch 6 teilbar.")
+elif nummer%5==0:
+  print("Die eingegebene Zahl " + str(nummer) +" ist durch fünf ohne Rest teilbar.")
+else:
+  print("Die eingegebene Zahl " + str(nummer) +" ist durch nicht durch 6 oder durch 5 teilbar.")
