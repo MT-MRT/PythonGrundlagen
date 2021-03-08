57eed2f5d8f696814242e575de2160c20a30e04f
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-08 20:21:29 +0100

    Create 09_zLösung.py

---
+++ 09_zLösung.py
@@ -0,0 +1,4 @@
+print("Lösung:")
+nummer= int(input("Gib hier eine Zahl ein: "))
+if nummer % 2 == 0:
+  print("Die eingegebene Zahl " + str(nummer) +" ist durch zwei ohne Rest teilbar und somit gerade.")
