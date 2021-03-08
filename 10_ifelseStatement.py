df734609b9695f5a622cee0edf9141f231216db7
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-08 21:29:10 +0100

    Create 10_ifelseStatement.py

---
+++ 10_ifelseStatement.py
@@ -0,0 +1,26 @@
+#In dieser Aufgabe geht es um das if else Statement
+#Aufgabe 10
+
+'''
+Das if Statement kann man zu einem else Statement erweitern. Das else ohne eine weitere Bedinung tritt dann in Kraft, wenn die Bedingung des if Statements nicht wahr ist. Knüpft man das else an eine weitere Bedingung, so wird dafür der Befehl "elif" genutzt, so kann man Fallunterscheidungen mit mehr als einem Fall erstellen.
+
+So erweitern wir das Bespiel um einen weiteren Fall ob die Zahl größer 20 ist und wenn keine dieser Bedinungen eintritt. 
+'''
+print("Gib hier eine beliebige Zahl ein:")
+zahl = int(input())
+if zahl>10 and zahl<=20:
+  print("Die eingegebene Zahl " + str(zahl) + " ist größer als 10.")
+elif zahl>20:
+  print("Die eingegebene Zahl " + str(zahl) + " ist größer als 20.")
+else: 
+  print("Die eingegebene Zahl " + str(zahl) + " ist kleiner oder gleich als 10.")
+
+'''
+In dem Beispiel wurde auch noch etwas neues eingeführt. Die Verknüfung von zwei Bedingungen mit "and". Dies ist ein sogenannter logischer Operator. Mit den logischen Operatoren "and" und "or" kann man Elementarvergleiche verknüpfen.
+and:
+Dies ist die und-Verknüpung, ist einer der beiden oder beide Elementarvergleiche die daran geknüpft sind falsch, so ist die ganze Bedingung falsch. Erst wenn beide damit verknüpften Vergleiche wahr sind ist die ganze Bedingung wahr. 
+or:
+Dies ist die oder-Verknüpung, ist einer der beiden oder beide Elementarvergleiche die daran geknüpft sind wahr, so ist die ganze Bedingung wahr. Erst wenn beide damit verknüpften Vergleiche falsch sind ist die ganze Bedingung falsch. 
+
+Aufgabe: Schreibe ein if-else Statment das prüft ob eine Zahl durch zwei und durch drei ohne Rest teilbar ist. Mit einem elseif soll zudem noch überprüft werden ob die Zahl durch fünf ohne Rest teilbar ist. Trifft beides nicht zu soll mit else ein kurzer Satz der dies darlegt ausgegeben werden.
+'''
