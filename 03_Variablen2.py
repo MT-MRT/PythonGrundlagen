7eb98616c450640bca0c71062a9ea59afff4467a
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-11 13:16:54 +0100

    Update 03_Variablen2.py

--- 03_Variablen2.py
+++ 03_Variablen2.py
@@ -1,5 +1,4 @@
-#In der letzen Aufgabe haben wie allgemin gelernt wie man eine Variable nutzt und die string Variable eingeführt, nun beschäftigen wir uns mit allen anderen möglichen Variablentypen
-#Aufgabe 3
+#In der letzen Aufgabe haben wie allgemeinn gelernt wie man eine Variable nutzt und die string Variable eingeführt, nun beschäftigen wir uns mit allen anderen möglichen Variablentypen
 
 """
 Python macht dem Nutzer die Nutzung von Variablen sehr leicht. So muss man beim Deklarieren der Variablen in Java erst den Datentyp der Variable definiern z.b "int zahl = 3;" , eine Typdeklaration wie diese ist in Python nicht nötig. 
