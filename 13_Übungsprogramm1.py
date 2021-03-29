0480398b0dd03102cadd5309429602956e3191a0
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-29 13:45:50 +0200

    Create 13_Übungsprogramm1.py

---
+++ 13_Übungsprogramm1.py
@@ -0,0 +1,11 @@
+#Dies ist ein Übungsprogramm um zu lernen die einzelnen Elemente zu verknüpfen und zu wiederholen
+
+'''
+Aufgabe: 
+Schreibe ein Programm, das eine Zahl über die Nutzereingabe annnimmt und alle ganzzahligen Teiler, welche die Zahl ohne Rest teilen, ausgibt. 
+
+Tipps:
+1.Erstelle eine Variable die als Teiler der eingegebenen Zahl dient. Nutze dann eine Schleife (zum Beispiel eine while Schleife) um diesen Teiler in jedem Schleifendurchlauf um eins zu erhöhen.
+2.Nutze den Modulo (%) um zu testen ob eine Zahl ein Teiler der eingegeben Zahl ist.
+3.Abhängig davon ob der Modulo null oder nicht null ist, soll ausgegeben werden, ob der Teiler die Zahl restlos teilt oder nicht.
+'''
