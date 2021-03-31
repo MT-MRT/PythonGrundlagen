6e9e848b675d2102ae1fd878c54353411f77a3b8
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-31 08:43:12 +0200

    Create 14_zLösung.py

---
+++ 14_zLösung.py
@@ -0,0 +1,27 @@
+import random
+
+def mittelwert(zahl1, zahl2, zahl3, anzahl):
+  mittelwert = (zahl1 + zahl2 + zahl3)/anzahl
+  return mittelwert
+
+def ausgabe(eingabe,info):
+  print(str(eingabe) + " " + info)
+
+print("Lösung:")
+
+zahl1 = float(input("Gebe hier die erste Zahl ein: "))
+zahl2 = float(input("Gebe hier die zweite Zahl ein: "))
+zahl3 = float(input("Gebe hier die dritte Zahl ein: "))
+anzahl = 3
+
+mittelwert_zahl=mittelwert(zahl1, zahl2, zahl3, anzahl)
+ausgabe(mittelwert_zahl, "Mittelwert")
+zufallszahl= random.randint(1, 100000)  
+ausgabe(zufallszahl, "Zufallszahl")
+
+fläche = mittelwert_zahl*zufallszahl
+ausgabe(fläche, "m^2 - Fläche")
+
+if fläche>=1000000:
+  fläche = fläche/1000000
+  ausgabe(fläche, "km^2 - Fläche") 
