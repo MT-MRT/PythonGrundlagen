602ddf6bd585fa2d18c12b8f501bb0be72341ecd
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-03-31 09:26:47 +0200

    Update 13_Übungsprogramm1.py

--- 13_Übungsprogramm1.py
+++ 13_Übungsprogramm1.py
@@ -1,7 +1,7 @@
 #Dies ist ein Übungsprogramm um zu lernen die einzelnen Elemente zu verknüpfen, der Fokus liegt hier auf Fallunterscheidungen und Schleifen
 '''
 Aufgabe: 
-Schreibe ein Programm, das eine Zahl über die Nutzereingabe annnimmt und alle ganzzahligen Teiler, welche die Zahl ohne Rest teilen, ausgibt. 
+Schreibe ein Programm, das eine Zahl über die Nutzereingabe annnimmt und alle ganzzahligen Teiler, welche die Zahl ohne Rest teilen, ausgibt.  Nutze die Tipps wenn Hilfe benötigt wird.
 
 Tipps:
 1.Erstelle eine Variable die als Teiler der eingegebenen Zahl dient. Nutze dann eine Schleife (zum Beispiel eine while Schleife) um diesen Teiler in jedem Schleifendurchlauf um eins zu erhöhen.
