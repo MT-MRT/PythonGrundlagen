ad6a4d4b5cc6c59a677ac4f506adea3732f2b55c
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 21:27:43 +0200

    Update 07_Funktionen2.py

--- Aufgaben/07_Funktionen2.py
+++ Aufgaben/07_Funktionen2.py
@@ -1,10 +1,11 @@
-#Dies ist die Fortsetzung von dem Abschnitt über Funktionen hier lernen wir wie man Parameter in eine Funktion einbindet
+print('\n\033[92m##### Tutorial #####\033[0m\n')
+############## Beschreibung ##############
+
+#Dies ist die Fortsetzung von dem Abschnitt über Funktionen. Hier lernen wir wie man Parameter in eine Funktion einbindet
 
 '''
 Bisher haben wir Funktionen nur dazu eingesetzt, dass sie das Nutzen von redundante Ausgabebefehle erleichtern. Nun lernen wir wie man Parameter in eine Funktion einbindet. 
-
 Ein Parameter ist eine Variable, welche die Eingabe aus dem Funktionsaufruf in den Programmcode im Funktionskörper überträgt. 
-
 Hier ein Beispiel:
 '''
 
@@ -15,11 +16,11 @@ halloName("Sam")
 halloName("Sabrina")
 
 '''
-Wichtig ist sobald ich angebe, dass ein Paramter in die Funktion eingebracht wird, muss der Wert dieses Parameters auch beim Funktionsaufruf übergeben werden. Probier es mal aus und lösche das "Sabrina", nun wird beim Ausführen eine Fehlermeldung kommen.
-
+Wichtig ist sobald man ein Parameter in eine Funktion einbringt, muss der Wert dieses Parameters auch beim Funktionsaufruf übergeben werden. Probier es mal aus und lösche das "Sabrina", nun wird beim Ausführen eine Fehlermeldung kommen.
 Wenn man mehr als ein Parameter in eine Funktion einbringen möchte, so werden diese durch Kommata getrennt hintereinander in die jeweilige Klammer geschrieben. Zum Beispiel: def hello(name, alter) und passend dazu beim Funktionsaufruf hallo("Sina", 23).
 
 Aufgabe: Schreibe eine Funktion die den Namen, den Studiengang und das Semester von einem Studenten annimmt und diese in einem sinvollen Satz ausgibt. Rufe diese Funktion dann mit drei verschiedenen Studenten auf. 
-
-Notiz: Denk daran, wenn man eine Nummer als Variable eingibt, diese bei der Ausgabe in einen string umzuwandeln.
 '''
+print('\n\033[92m##### Lösung Studierende #####\033[0m\n')
+############## Ab hier kannst du deine Lösung schreiben ##############
+
