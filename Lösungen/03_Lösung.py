e83fb40801435a42d360008db37b23524dc40f85
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 22:07:26 +0200

    Update 03_Lösung.py

--- Lösungen/03_Lösung.py
+++ Lösungen/03_Lösung.py
@@ -1,4 +1,6 @@
-print("Lösung:")
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Mögliche Lösung ##############
+
 roboter_name = "Lenn"
 anzahl_bauteile = 135
 geschwindigkeit_hoechst = 0.034
@@ -9,4 +11,4 @@ print(geschwindigkeit_hoechst)
 
 print("Der Name des Roboters ist " + roboter_name + ".")
 print(roboter_name + " wurde aus " + str(anzahl_bauteile) + " Bauteilen gebaut.")
-print("Der Roboter besteht zwar nur aus " + str(anzahl_bauteile) + " besteht ist die Höchstgeschwindigkeit von " + roboter_name + " " + str(geschwindigkeit_hoechst) + "km/h.")
+print("Der Roboter besteht zwar nur aus " + str(anzahl_bauteile) + " Bauteilen, hat aber eine Höchstgeschwindigkeit von " + roboter_name + " " + str(geschwindigkeit_hoechst) + "km/h.")
