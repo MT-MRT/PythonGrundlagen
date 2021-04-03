a261f1afd79e7e9d6c9ea5dd59883b10016f7240
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 22:08:31 +0200

    Update 04_Lösung.py

--- Lösungen/04_Lösung.py
+++ Lösungen/04_Lösung.py
@@ -1,4 +1,6 @@
-print("So oder so ähnlich sollte deine Lösung aussehen:")
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Mögliche Lösung ##############
+
 nummer4 = 45
 nummer5 = nummer4 - 5
 print("nummer4:" + str(nummer4))
@@ -6,7 +8,7 @@ print("nummer5:" + str(nummer5))
 print(max(nummer4, nummer5 + 7)) 
 print("nummer4:" + str(nummer4))
 print("nummer5:" + str(nummer5))
-print("Dir sollte auffallen, das mit dem Addieren von 7 zu nummer5, innerhalb der print Operation, der Wert der eigentlichen Variable nicht verändert wird. Den Wert der Variable an sich hättest du z.B so verändert: ")
+print("Dir sollte auffallen, das mit dem Addieren von 7 zu nummer5, innerhalb der print Operation, der Wert der eigentlichen Variable nicht verändert wird. Den Wert der Variable an sich würde man z.B so verändern: ")
 nummer4 = 45
 nummer5 = nummer4 - 5
 print("nummer4:" + str(nummer4))
