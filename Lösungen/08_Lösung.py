ea6938f1ffabeca91bad7881cab8b1cdc040c99e
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 22:11:28 +0200

    Update 08_Lösung.py

--- Lösungen/08_Lösung.py
+++ Lösungen/08_Lösung.py
@@ -1,12 +1,15 @@
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Mögliche Lösung ##############
+
 def rechner(zahl):
   return zahl*zahl
 
-print("Hier eine mögliche Lösung:")
 nummer = float( input( "Bitte gebe die Zahl ein die die quadriert werden soll: "))
 print(rechner(nummer))
 
 '''
-#hier eine weitere Möglichkeit: mach die ''' ''' weg und schau sie dir an  
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Hier noch eine weitere mögliche Lösung, zum Anschauen einefach die '''...''' entfernen ##############
 
 def rechner(zahl):
   quadrieren = zahl*zahl
