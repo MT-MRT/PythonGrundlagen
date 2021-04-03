d3dd34a966291655bac794be0caf60c987ed3d05
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 22:11:42 +0200

    Update 07_Lösung.py

--- Lösungen/07_Lösung.py
+++ Lösungen/07_Lösung.py
@@ -4,7 +4,6 @@ print('\n\033[92m##### Lösung #####\033[0m\n')
 def student(name, studiengang, semester):
   print(name + " studiert " + studiengang + " und ist in Semester " + str(semester) + ".")
 
-print("Hier eine mögliche Lösung:")
 print("Studentenliste:")
 student("Hans", "Mathe" , 3)
 student("Sina", "Maschienenbau", "7")
