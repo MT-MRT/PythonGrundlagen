1fd0f0a826cde015fef0d0f553b2f0fd0ac6b6ea
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 22:13:10 +0200

    Update 09_Lösung.py

--- Lösungen/09_Lösung.py
+++ Lösungen/09_Lösung.py
@@ -1,4 +1,6 @@
-print("Lösung:")
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Mögliche Lösung ##############
+
 nummer= int(input("Gib hier eine Zahl ein: "))
 if nummer % 2 == 0:
   print("Die eingegebene Zahl " + str(nummer) +" ist durch zwei ohne Rest teilbar und somit gerade.")
