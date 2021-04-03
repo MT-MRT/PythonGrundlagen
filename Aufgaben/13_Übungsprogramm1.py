14a504a786995654a6c15b30416e750a4f9dd4ba
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 21:59:26 +0200

    Update 13_Übungsprogramm1.py

--- Aufgaben/13_Übungsprogramm1.py
+++ Aufgaben/13_Übungsprogramm1.py
@@ -1,10 +1,15 @@
+print('\n\033[92m##### Tutorial #####\033[0m\n')
+############## Beschreibung ##############
+
 #Dies ist ein Übungsprogramm um zu lernen die einzeln gelerneten Elemente zu verknüpfen, der Fokus liegt hier auf Fallunterscheidungen und Schleifen
 '''
 Aufgabe: 
-Schreibe ein Programm, das eine Zahl über die Nutzereingabe annnimmt und alle ganzzahligen Teiler, welche die Zahl ohne Rest teilen, ausgibt.  Nutze die Tipps wenn Hilfe benötigt wird.
+Schreibe ein Programm, welches eine Zahl über die Nutzereingabe annnimmt und alle ganzzahligen Teiler, welche die Zahl ohne Rest teilen, ausgibt. Nutze die Tipps wenn Hilfe benötigt wird.
 
 Tipps:
-1.Erstelle eine Variable die als Teiler der eingegebenen Zahl dient. Nutze dann eine Schleife (zum Beispiel eine while Schleife) um diesen Teiler in jedem Schleifendurchlauf um eins zu erhöhen.
-2.Nutze den Modulo (%) um zu testen ob eine Zahl ein Teiler der eingegeben Zahl ist.
-3.Abhängig davon ob der Modulo null oder nicht null ist, soll ausgegeben werden, ob der Teiler die Zahl restlos teilt oder nicht.
+1. Erstelle eine Variable die als Teiler der eingegebenen Zahl dient. Nutze dann eine Schleife (zum Beispiel eine while Schleife) um diesen Teiler in jedem Schleifendurchlauf um eins zu erhöhen.
+2. Nutze den Modulo (%) um zu testen, ob eine Zahl ein Teiler der eingegeben Zahl ist.
+3. Abhängig davon ob der Modulo null oder nicht null ist, soll ausgegeben werden, ob der Teiler die Zahl restlos teilt oder nicht.
 '''
+print('\n\033[92m##### Lösung Studierende #####\033[0m\n')
+############## Ab hier kannst du deine Lösung schreiben ##############
