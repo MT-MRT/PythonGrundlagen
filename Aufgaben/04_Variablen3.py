51b06d7e9e5398be01ef6ae58782ac303c8c2d94
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 21:09:49 +0200

    Update 04_Variablen3.py

--- Aufgaben/04_Variablen3.py
+++ Aufgaben/04_Variablen3.py
@@ -1,8 +1,11 @@
-#Hier geht es um das Arbeiten mit Zahlen
+print('\n\033[92m##### Tutorial #####\033[0m\n')
+############## Beschreibung ##############
+
+#Hier geht es um das weiterführende Arbeiten mit Zahlen
 
 '''
-Nahdem wir Zahlen als Variablen deklariert und ausgegeben haben gehen wir nun einen Schritt weiter und lernen diese zu verrechnen oder im Nachgang zu verändern.
-Einfache Fomelzeichen können direkt in der Deklaration oder beim Ausgeben der Zahlen genutzt werden. Wie dies funktioniert siehst du an den Beispielen nummer1,  nummer2 und nummer3.
+Nahdem wir Zahlen als Variablen deklariert und ausgegeben haben, gehen wir nun einen Schritt weiter und lernen diese zu verrechnen oder im Nachgang zu verändern.
+Einfache Fomelzeichen können direkt in der Deklaration oder beim Ausgeben der Zahlen genutzt werden. Wie dies funktioniert siehst man an den Beispielen nummer1,  nummer2 und nummer3.
 Dabei kannst du alle Standartrechenarten nutzen:
 Addition: +
 Substraktion: -
@@ -12,8 +15,8 @@ Zudem gibt es eine Operation names Modulo, dies ist eine Divison bei der der Res
 Praktisch ist machmal auch den Betrag einer Zahl ausgeben zu lassen: print(abs(...))
 Als leztes gibt es noch einen Operator der immer die größere Zahl von den Eingaben ausgibt: print(max(...,...)) oder auch die kleiner Zahl: print(min(...,...))
 
-Aufgabe: Lege die Zahlen nummer4 und nummer5 an. nummer3 soll den Wert 45 haben und nummer 5 immer einen um 5 geringeren Wert als nummer1. 
-Gibt nun den Wert beider Zahlen aus. Gibt danach die gößere Zahlen der Beiden aus, nachdem du zu nummer5 innerhalb der print Funktion mit 7 addiert hast. Gibt nun nochmal den Wert beider Zahlen aus. Was fällt dir auf?
+Aufgabe: Lege die Zahlen nummer4 und nummer5 an. Dabei soll nummer3 den Wert 45 haben und nummer 5 immer einen um 5 geringeren Wert als nummer1. 
+Erstelle dann eine Ausgabe beider Zahlen. Gebe dann die gößere Zahl der Beiden aus, nachdem die nummer5 innerhalb der print Funktion mit 7 addiert wurde. Erstelle nun eine zweite Ausgabe der beiden Zahlen. Was fällt auf?
 '''
 
 nummer1 = 7.45 - 45
@@ -24,3 +27,6 @@ print("Dies ist nummer1: " + str(nummer1))
 print("Dies ist ein Beispiel für die Nutzung des Modulo: " + str(nummer3))
 print(abs(nummer1))
 print(max(nummer2, 24))
+
+print('\n\033[92m##### Lösung Studierende #####\033[0m\n')
+############## Ab hier kannst du deine Lösung schreiben ##############
