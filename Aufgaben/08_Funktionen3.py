ba19f82194e4ebca5247047466b9c936e2002808
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-03 21:32:18 +0200

    Update 08_Funktionen3.py

--- Aufgaben/08_Funktionen3.py
+++ Aufgaben/08_Funktionen3.py
@@ -1,17 +1,19 @@
-#Dies ist die Fortsetzung von dem Abschnitt über Funktionen hier lernen wir wie man eine Rückgabe von Funktionen erhält 
+print('\n\033[92m##### Tutorial #####\033[0m\n')
+############## Beschreibung ##############
+
+#Dies ist die Fortsetzung von dem Abschnitt über Funktionen. Hier lernen wir wie man eine Rückgabe von Funktionen erhält. 
 
 '''
 In den letzten Aufgaben haben wir immer eine direkte Ausgabe in der Funktion gehabt. Damit wir aber auch mit den Werten, die von der Funtkion erzeugt werden, weiterarbeiten können brauchen wir das return Statement.
-
 Das return Statement steht am Ende des Funktionskörpers. Nach dem return Statement steht die Variable, welche wir zurückgeben wollen. Alles was in der nächsten Zeile nach einem return Statement geschrieben wird, wird nicht mehr vom Commpiler gelesen. 
 Die zurückgegebene Variable kann dann entweder direkt ausgegeben oder es kann weiter mit ihr gearbeitet werden.
-
 Hier ein Beispiel:
 '''
 
 def addition3(zahl):
   return zahl + 3
   print("Dies wir nicht ausgegeben, da es in der Zeile unter dem return steht.")
+  
 nummer = 34
 volls_addition = addition3(nummer) + 3
 print(volls_addition)
@@ -19,3 +21,5 @@ print(volls_addition)
 '''
 Aufgabe: Schreibe ein Programm, welches eine Zahl durch Eingabe des Benutzers annimmt und diese an eine Funktion übergibt. Die Funktion soll die Zahl quadrieren und das Ergebnis dessen zurückgeben. Nach der Rückgabe soll das Ergebnis ausgegeben werden.
 '''
+print('\n\033[92m##### Lösung Studierende #####\033[0m\n')
+############## Ab hier kannst du deine Lösung schreiben ##############
