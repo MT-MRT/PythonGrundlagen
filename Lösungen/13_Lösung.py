87a9e554097961d9714ee2585fde5c37899886fe
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-04 22:14:41 +0200

    Update 13_Lösung.py

--- Lösungen/13_Lösung.py
+++ Lösungen/13_Lösung.py
@@ -1,1 +1,19 @@
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Mögliche Lösung ##############
 
+zahl = 0
+geradeZahlen = [0,0,0,0]
+i = 0
+
+while i < 4:
+
+    zahl = int(input("Gebe bitte eine gerade Zahl ein: "))
+
+    if zahl % 2 != 0:
+        print("Diese Zahl ist nicht gerade!")
+
+    else:
+     geradeZahlen[i]=zahl
+     i+=1
+
+    print("\n" + str(geradeZahlen) + "\n")
