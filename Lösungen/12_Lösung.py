ffcbe28a832664c8ef218dc9a677607af80bf881
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-04 21:35:15 +0200

    Update 12_Lösung.py

--- Lösungen/12_Lösung.py
+++ Lösungen/12_Lösung.py
@@ -1,1 +1,13 @@
+print('\n\033[92m##### Lösung #####\033[0m\n')
+############## Mögliche Lösung ##############
+namen=["Tom", "Tina", "Simone", "Ted", "Charlie"]
 
+print("Erste Ausgabe:")
+for person in namen:
+  print(person)
+
+länge=len(namen)
+
+print("\nZweite Ausgabe:")
+for j in range(0, länge):
+  print(str(j+1) +". "+ namen[j])
