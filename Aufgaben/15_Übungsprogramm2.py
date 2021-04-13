578499cc07c5c618385119a48a73efc5bd7bf693
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-13 15:34:05 +0200

    Update 15_Übungsprogramm2.py

--- Aufgaben/15_Übungsprogramm2.py
+++ Aufgaben/15_Übungsprogramm2.py
@@ -10,7 +10,7 @@ Aufgabe:
 Schreibe ein Programm das drei float Werte über die Nutzereingabe annimmt und aus diesen erst mithilfe der selbstgeschriebenen Funktion "mittelwert" den Mittelwert der drei Zahlen bildet. 
 Das Ergebnis soll dann über eine weitere Funktion namens "ausgabe" das Ergebnis und ein erklärendes Wort dazu ausgeben (Beispielausgabe: 345,5 - Mittelwert).
 
-Weiterführend soll eine Zufallszahl zwischen 1 und 100000 generiert werden. Dafür muss das Modul "random" importiert werden. Zum Generieren der Zufallszahl wird dann die in random vorhandene Funktion ".randint()" genutzt, diese wird mit "random.radint()"aufgerufen. In den Klammern von radiant werden zurerst der Anfangswert und dann der Endwert der Menge, aus der die Zufallszahl generiet werden soll, übergeben (Beispiel das Erstellen eines zuälligen Würfels: würfel = random.radiant(1,6)).
+Weiterführend soll eine Zufallszahl zwischen 1 und 100000 generiert werden. Dafür muss das Modul "random" importiert werden und die Funktion ".radiant()"genutzt werden.
 
 Sobald die Zufallszahl generiert wurde, soll diese mit dem Mittelwert zu einer Fläche [m^2] multipliziert werden. Sowohl die generierte Zufallszahl, als auch die entstandene Fläche sollen über die Funktion "ausgabe" mit Wert und einem erklärenden Wort ausgegeben werden. 
 
