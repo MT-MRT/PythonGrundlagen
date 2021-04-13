01d8db95f1453d0911ef09c7cd0f820e8c81a276
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-13 13:45:59 +0200

    Update 11_Listen.py

--- Aufgaben/11_Listen.py
+++ Aufgaben/11_Listen.py
@@ -8,25 +8,35 @@ Man kann Listen in Python nutzen um Elemente beliebiger Datentypen geordnet zu v
 Hier ein Beispiel:
 '''
 studium = ["Maschinenbau","Bachelor",9,6]
-print(studium)
-print("Stelle 0: " + studium[0])
-print("Stelle 1: " + studium[1])
-print("Stelle -1: " + str(studium[-1]))
-print("Stelle -2: " + str(studium[-2]))
-studium[1]="Master"
-print("Neuer Wert der Stelle 1: " + studium[1])
-print("Die Liste Studium hat " + str(len(studium)) + " Elemente")
 '''
 Wie hier schon zu sehen, kann man eine Liste durch einen Namen der Liste und dann eckige Klammern mit den Elementen darin anlegen. Um dann wieder auf die Elemente zuzugreifen wird die Stelle in der Liste, in welcher das Element steht, in Kombination mit dem Namen der Liste angegeben (Listenname[Stelle]). Es ist dabei zu beachten, dass das erste Element einer Liste an der Stelle 0 (nicht 1!) steht.
-
+'''
+print("Stelle 0: " + studium[0])
+print("Stelle 1: " + studium[1])
+'''
 Wenn man eine längere Liste hat und auf eines der hinteren Elemente zugreifen möchte, so kann man auch von hinten auf die Liste zugreifen. Dazu werden negative Stellenangaben genutzt, dabei ist es wichtig zu wissen, dass [-1] das letzte Element ist.
-
+'''
+print("Stelle -1: " + str(studium[-1]))
+print("Stelle -2: " + str(studium[-2]))
+'''
 Um die ganze Liste auszugeben kann der Listenname einfach in einen print Befehl geschrieben werden. Soll nur ein Element ausgegeben werden, so muss die Stelle des Elements mit angegeben werden. 
-
+'''
+print(studium)
+'''
 Wenn man ein Element einer Liste über die Stelle in der Liste aufruft, so kann dieses wie jede andere Variable genutzt werden. 
-
+'''
+studium[1]="Master"
+print("Neuer Wert der Stelle 1: " + studium[1])
+'''
 Nützlich ist auch oft das Bestimmen der Anzahl der Elemente in einer Liste mit der Funktion len(Listenname).
- 
+'''
+print("Die Liste Studium hat " + str(len(studium)) + " Elemente")
+'''
+Man kann auch nachträglich noch Elemente an eine Liste anhängen dazu wird ".append()" genutzt
+'''
+studium.append("Abschluss")
+print(studium)
+'''
 Aufgabe: 
 Erstelle eine Liste welche eine Einkaufsliste enthält. Fülle die Liste abwechselnd mit dem Lebensmittel welches gekauft werden soll und im nächsten Element der Anzahl dessen. Schreibe mindestens drei Lebensmittel auf die Einkaufsliste.
 Ändere nun nachträglich ein Lebensmittel und die Anzahl davon. 
@@ -34,4 +44,3 @@ Für die Ausgabe soll eine Funktion geschrieben werden, die das Lebensmittel und
 '''
 print('\n\033[92m##### Lösung Studierende #####\033[0m\n')
 ############## Ab hier kannst du deine Lösung schreiben ##############
-
