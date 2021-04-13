8b79fc0245df2f4c004ebd8ee772519e3ff5a251
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-13 14:51:08 +0200

    Create import.py

---
+++ Aufgaben/import.py
@@ -0,0 +1,23 @@
+print('\n\033[92m##### Tutorial #####\033[0m\n')
+############## Beschreibung ##############
+
+#hier wird der Import von externen Paketen erklärt
+
+'''
+Es gibt die Möglichkeit externe Module in das eigene Programm einzubinden. Dafür muss man die Module mit dem Keyword "import" importieren, dieses wird immer als oberster Befehl in ein Programm geschrieben. Hat man ein Modul importiert, so kann man die darin vorhandenen Funktionen nutzen. Da die Funktion aus dem importierten  Modul kommt, kann man die Funktion nicht einfach nur mit dem Funktionsnamen aufrufen, sondern muss noch den Modulnamen davorschreiben.
+Hier ein Beispiel:
+''' 
+import math
+print("PI ist: " + str(math.pi))
+'''
+Wie die Funktionen in den importierten Modulen heißen und was diese an Parametern brauchen, findet man am einfachsten durch eine kurze Google-Suche heraus.
+
+Aufgabe:
+Importiere die Module "time" und "random".
+In "time" gibt es die Funktion ".sleep()" mit der man das Fortlaufen des Programms verzögert. Als Parameter wird hier eine Zeit in Sekunden angegeben.
+Mit "random" kann man Zufallszahlen erstellen. Wir nutzen hier die Funktion ".radint". In den Klammern von radiant werden zuerst der Anfangswert und dann der Endwert der Menge, aus der die Zufallszahl generiet werden soll, übergeben (Beispiel das Erstellen eines zufälligen Würfels: würfel = random.radiant(1,6)).
+Nutze nun die ".radiant" Funktion und erstelle eine Zufallszahl zwischen zwei und sieben, gebe diese im Anschluss aus.
+Schreibe dann eine for-Schleife, die sooft wie die Zufallszahl hoch ist durchläuft. In der Schleife befinden sich zwei Ausgaben, die erste gibt den Durchlauf der Schleife an (also die Höhe der Zählvariablen). Die zweite Ausgabe soll mit ".sleep" um 3 Sekunden verzögert kommen und "Diese Ausgabe ist verzögert" ausgeben.
+'''
+print('\n\033[92m##### Lösung Studierende #####\033[0m\n')
+############## Ab hier kannst du deine Lösung schreiben ##############
