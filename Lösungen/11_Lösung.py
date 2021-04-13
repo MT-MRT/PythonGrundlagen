2276f7b03d537230bd051b0637f3e57b49beea84
ClaraFahrenbach <74987946+ClaraFahrenbach@users.noreply.github.com>
2021-04-13 14:02:47 +0200

    Update 11_Lösung.py

--- Lösungen/11_Lösung.py
+++ Lösungen/11_Lösung.py
@@ -7,8 +7,11 @@ def ausgabe(lebensmittel, anzahl):
 einkauf=["Banane",2,"Brot",1,"Öl",1,"Nudeln",2]
 einkauf[4]="Gurke"
 einkauf[5]= 2
+einkauf.append("Senf")
+einkauf.append(1)
 print(str(len(einkauf)) + " Dinge müssen gekauft werden")
 ausgabe(einkauf[0],einkauf[1])
 ausgabe(einkauf[2],einkauf[3])
 ausgabe(einkauf[4],einkauf[5])
 ausgabe(einkauf[6],einkauf[7])
+ausgabe(einkauf[8],einkauf[9])
